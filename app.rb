c7d837a94e0f8b3fa0857f541c0c6861c2816fd0
Gilbert Torchon <gilbert@noukod.com>
2019-01-07 17:17:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/name' do
     "My name is Gilbert Torchon"
   end
+
+  get '/hometown' do
+    "My name is Gilbert Torchon"
+  end
+  
 end
